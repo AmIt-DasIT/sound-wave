5b173dba19af58af0d7c3cd125d4defe769ac357
AmitDasIT <amit@distronix.in>
2025-05-27 19:02:49 +0530

    tooltip updated now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -13,13 +10,11 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider>
       <ColorProvider>
-        <TooltipProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-            </Routes>
-          </BrowserRouter>
-        </TooltipProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+          </Routes>
+        </BrowserRouter>
       </ColorProvider>
     </ThemeProvider>
   </QueryClientProvider>
